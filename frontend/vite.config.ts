7bf89cd308f8af79ddaa7870eafd06c0850ec29d
Nikita Voynov <voynov@nikscorp.com>
2026-04-26 16:20:14 +0400

    frontend fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,4 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
